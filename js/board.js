70b26c32487a86c2b867e3f6b1816444ffce84c1
Jordan Hall <jordanhall.1995@yahoo.com>
2017-04-06 09:00:46 -0700

    bombs fadeout and can place another one after gone

--- js/board.js
+++ js/board.js
@@ -9,20 +9,24 @@ container.className = "container"
 
 body.append(container)
 
-
-function createBoard (pos) {
+function createBoard () {
   var mySquares = document.createElement('div')
   mySquares.className = 'squares'
   container.append(mySquares)
   return mySquares
 }
-
 function createBlocks () {
   var myBlock = document.createElement('div')
   myBlock.className = 'block'
   myBlock.style.backgroundColor = 'brown'
   return myBlock
 }
+function createBombs () {
+  var myBomb = document.createElement('div')
+  myBomb.className = "bomb"
+  return myBomb
+}
+
 // 5x5 with outside blocked so 4x4 inside with 1 colums and 1 row
 // 5 = 1 , 7 = 2 row or colum 9 = 3
 var gameX = 13 // greater than 4
@@ -34,7 +38,7 @@ var trackSquares = []
 var placeBlocks = []
 var getSquare = []
 var roadBlocks = []
-
+var bombDown = []
 
 // create board with gameX is how many squares left and right
 // gameY is how many squares up and down
@@ -78,7 +82,7 @@ for(var i = 0; i < gameX * gameY; i += 1) {
     getSquare.push(trackSquares[i])
   }
 }
-for(var i = 0; i < gameX * gameY; i += 2){
+for(var i = 0; i < gameX * gameY; i += 2) {
   if(i > gameX * 2 && i < gameX * 3 - 1) {
     roadBlocks.push(trackSquares[i])
   }
@@ -89,6 +93,9 @@ for(var i = 0; i < gameX * gameY; i += 2){
     roadBlocks.push(trackSquares[i])
   }
 }
+for(var i = 0; i < getSquare.length; i += 1) {
+  bombDown.push(createBombs(i))
+}
 
 container.append(guy1)
 container.append(guy2)
@@ -97,6 +104,7 @@ var guyX = 462
 var guyY = 292
 var guy2X = 42
 var guy2Y = 42
+var bombPosX = 13
 
 var keyState = {}
 document.addEventListener('keydown',function(e){
@@ -157,7 +165,7 @@ function moveGuy1() {
 }
 function moveGuy2() {
   if(keyState[65]){
-    guy2X-= 2
+    guy2X -= 2
     if (guy2X <= 40) {
       guy2X += 2
     }
@@ -191,415 +199,428 @@ function moveGuy2() {
 bomb = {
   is: document.createElement('div'),
   number: 1,
-  power: 1
+  power: 10
 }
+
 
 plantBomb = function () {
   if(keyState[32] || keyState[13] || keyState[18]) {
     bomb.is.className = 'bomb'
+    var bombOut = $('<div>')
+    bombOut.addClass('bomb')
     if (guyY < 60) {
       if (guyX < 60){
-        getSquare[0].append(bomb.is)
+        getSquare[0].append(bombDown[0])
       }
       if (guyX > 60 && guyX < 107){
-          getSquare[1].append(bomb.is)
+          getSquare[1].append(bombDown[1])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[2].append(bomb.is)
+        getSquare[2].append(bombDown[2])
       }
       if (guyX > 147 && guyX < 187) {
-        getSquare[3].append(bomb.is)
+        getSquare[3].append(bombDown[3])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[4].append(bomb.is)
+        getSquare[4].append(bombDown[4])
       }
       if (guyX > 227 && guyX < 267) {
-        getSquare[5].append(bomb.is)
+        getSquare[5].append(bombDown[5])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[6].append(bomb.is)
+        getSquare[6].append(bombDown[6])
       }
       if (guyX > 307 && guyX < 347) {
-        getSquare[7].append(bomb.is)
+        getSquare[7].append(bombDown[7])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[8].append(bomb.is)
+        getSquare[8].append(bombDown[8])
       }
       if (guyX > 387 && guyX < 427) {
-        getSquare[9].append(bomb.is)
+        getSquare[9].append(bombDown[9])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[10].append(bomb.is)
+        getSquare[10].append(bombDown[10])
       }
     }
     if (guyY > 60 && guyY < 107) {
       if (guyX < 60) {
-        getSquare[11].append(bomb.is)
+        getSquare[11].append(bombDown[11])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[12].append(bomb.is)
+        getSquare[12].append(bombDown[12])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[13].append(bomb.is)
+        getSquare[13].append(bombDown[13])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[14].append(bomb.is)
+        getSquare[14].append(bombDown[14])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[15].append(bomb.is)
+        getSquare[15].append(bombDown[15])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[16].append(bomb.is)
+        getSquare[16].append(bombDown[16])
       }
     }
     if (guyY > 107 && guyY < 147) {
       if (guyX < 60){
-        getSquare[17].append(bomb.is)
+        getSquare[17].append(bombDown[17])
       }
       if (guyX > 60 && guyX < 107){
-          getSquare[18].append(bomb.is)
+          getSquare[18].append(bombDown[18])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[19].append(bomb.is)
+        getSquare[19].append(bombDown[19])
       }
       if (guyX > 147 && guyX < 187) {
-        getSquare[20].append(bomb.is)
+        getSquare[20].append(bombDown[20])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[21].append(bomb.is)
+        getSquare[21].append(bombDown[21])
       }
       if (guyX > 227 && guyX < 267) {
-        getSquare[22].append(bomb.is)
+        getSquare[22].append(bombDown[22])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[23].append(bomb.is)
+        getSquare[23].append(bombDown[23])
       }
       if (guyX > 307 && guyX < 347) {
-        getSquare[24].append(bomb.is)
+        getSquare[24].append(bombDown[24])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[25].append(bomb.is)
+        getSquare[25].append(bombDown[25])
       }
       if (guyX > 387 && guyX < 427) {
-        getSquare[26].append(bomb.is)
+        getSquare[26].append(bombDown[26])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[27].append(bomb.is)
+        getSquare[27].append(bombDown[27])
       }
     }
     if (guyY > 147 && guyY < 187) {
       if (guyX < 60) {
-        getSquare[28].append(bomb.is)
+        getSquare[28].append(bombDown[28])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[29].append(bomb.is)
+        getSquare[29].append(bombDown[29])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[30].append(bomb.is)
+        getSquare[30].append(bombDown[30])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[31].append(bomb.is)
+        getSquare[31].append(bombDown[31])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[32].append(bomb.is)
+        getSquare[32].append(bombDown[32])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[33].append(bomb.is)
+        getSquare[33].append(bombDown[33])
       }
     }
     if (guyY > 187 && guyY < 227) {
       if (guyX < 60){
-        getSquare[34].append(bomb.is)
+        getSquare[34].append(bombDown[34])
       }
       if (guyX > 60 && guyX < 107){
-          getSquare[35].append(bomb.is)
+          getSquare[35].append(bombDown[35])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[36].append(bomb.is)
+        getSquare[36].append(bombDown[36])
       }
       if (guyX > 147 && guyX < 187) {
-        getSquare[37].append(bomb.is)
+        getSquare[37].append(bombDown[37])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[38].append(bomb.is)
+        getSquare[38].append(bombDown[38])
       }
       if (guyX > 227 && guyX < 267) {
-        getSquare[39].append(bomb.is)
+        getSquare[39].append(bombDown[39])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[40].append(bomb.is)
+        getSquare[40].append(bombDown[40])
       }
       if (guyX > 307 && guyX < 347) {
-        getSquare[41].append(bomb.is)
+        getSquare[41].append(bombDown[41])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[42].append(bomb.is)
+        getSquare[42].append(bombDown[42])
       }
       if (guyX > 387 && guyX < 427) {
-        getSquare[43].append(bomb.is)
+        getSquare[43].append(bombDown[43])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[44].append(bomb.is)
+        getSquare[44].append(bombDown[44])
       }
     }
     if (guyY > 227 && guyY < 267) {
       if (guyX < 60) {
-        getSquare[45].append(bomb.is)
+        getSquare[45].append(bombDown[45])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[46].append(bomb.is)
+        getSquare[46].append(bombDown[46])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[47].append(bomb.is)
+        getSquare[47].append(bombDown[47])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[48].append(bomb.is)
+        getSquare[48].append(bombDown[48])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[49].append(bomb.is)
+        getSquare[49].append(bombDown[49])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[50].append(bomb.is)
+        getSquare[50].append(bombDown[50])
       }
     }
     if (guyY > 267 && guyY < 307) {
       if (guyX < 60){
-        getSquare[51].append(bomb.is)
+        getSquare[51].append(bombDown[51])
       }
       if (guyX > 60 && guyX < 107){
-          getSquare[52].append(bomb.is)
+          getSquare[52].append(bombDown[52])
       }
       if (guyX > 107 && guyX < 147) {
-        getSquare[53].append(bomb.is)
+        getSquare[53].append(bombDown[53])
       }
       if (guyX > 147 && guyX < 187) {
-        getSquare[54].append(bomb.is)
+        getSquare[54].append(bombDown[54])
       }
       if (guyX > 187 && guyX < 227) {
-        getSquare[55].append(bomb.is)
+        getSquare[55].append(bombDown[55])
       }
       if (guyX > 227 && guyX < 267) {
-        getSquare[56].append(bomb.is)
+        getSquare[56].append(bombDown[56])
       }
       if (guyX > 267 && guyX < 307) {
-        getSquare[57].append(bomb.is)
+        getSquare[57].append(bombDown[57])
       }
       if (guyX > 307 && guyX < 347) {
-        getSquare[58].append(bomb.is)
+        getSquare[58].append(bombDown[58])
       }
       if (guyX > 347 && guyX < 387) {
-        getSquare[59].append(bomb.is)
+        getSquare[59].append(bombDown[59])
       }
       if (guyX > 387 && guyX < 427) {
-        getSquare[60].append(bomb.is)
+        getSquare[60].append(bombDown[60])
       }
       if (guyX > 427 && guyX < 467) {
-        getSquare[61].append(bomb.is)
+        getSquare[61].append(bombDown[61])
       }
     }
+    $(".bomb").fadeOut(3000, function () {
+      $(".bomb").fadeIn(1, function (){
+        $(this).remove()
+      })
+    })
   }
   else if(keyState[16]) {
     bomb.is.className = 'bomb'
     if (guy2Y < 60) {
       if (guy2X < 60){
-        getSquare[0].append(bomb.is)
+        getSquare[0].append(bombDown[0])
       }
       if (guy2X > 60 && guy2X < 107){
-          getSquare[1].append(bomb.is)
+          getSquare[1].append(bombDown[1])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[2].append(bomb.is)
+        getSquare[2].append(bombDown[2])
       }
       if (guy2X > 147 && guy2X < 187) {
-        getSquare[3].append(bomb.is)
+        getSquare[3].append(bombDown[3])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[4].append(bomb.is)
+        getSquare[4].append(bombDown[4])
       }
       if (guy2X > 227 && guy2X < 267) {
-        getSquare[5].append(bomb.is)
+        getSquare[5].append(bombDown[5])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[6].append(bomb.is)
+        getSquare[6].append(bombDown[6])
       }
       if (guy2X > 307 && guy2X < 347) {
-        getSquare[7].append(bomb.is)
+        getSquare[7].append(bombDown[7])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[8].append(bomb.is)
+        getSquare[8].append(bombDown[8])
       }
       if (guy2X > 387 && guy2X < 427) {
-        getSquare[9].append(bomb.is)
+        getSquare[9].append(bombDown[9])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[10].append(bomb.is)
+        getSquare[10].append(bombDown[10])
       }
     }
     if (guy2Y > 60 && guy2Y < 107) {
       if (guy2X < 60) {
-        getSquare[11].append(bomb.is)
+        getSquare[11].append(bombDown[11])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[12].append(bomb.is)
+        getSquare[12].append(bombDown[12])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[13].append(bomb.is)
+        getSquare[13].append(bombDown[13])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[14].append(bomb.is)
+        getSquare[14].append(bombDown[14])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[15].append(bomb.is)
+        getSquare[15].append(bombDown[15])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[16].append(bomb.is)
+        getSquare[16].append(bombDown[16])
       }
     }
     if (guy2Y > 107 && guy2Y < 147) {
       if (guy2X < 60){
-        getSquare[17].append(bomb.is)
+        getSquare[17].append(bombDown[17])
       }
       if (guy2X > 60 && guy2X < 107){
-          getSquare[18].append(bomb.is)
+          getSquare[18].append(bombDown[18])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[19].append(bomb.is)
+        getSquare[19].append(bombDown[19])
       }
       if (guy2X > 147 && guy2X < 187) {
-        getSquare[20].append(bomb.is)
+        getSquare[20].append(bombDown[20])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[21].append(bomb.is)
+        getSquare[21].append(bombDown[21])
       }
       if (guy2X > 227 && guy2X < 267) {
-        getSquare[22].append(bomb.is)
+        getSquare[22].append(bombDown[22])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[23].append(bomb.is)
+        getSquare[23].append(bombDown[23])
       }
       if (guy2X > 307 && guy2X < 347) {
-        getSquare[24].append(bomb.is)
+        getSquare[24].append(bombDown[24])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[25].append(bomb.is)
+        getSquare[25].append(bombDown[25])
       }
       if (guy2X > 387 && guy2X < 427) {
-        getSquare[26].append(bomb.is)
+        getSquare[26].append(bombDown[26])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[27].append(bomb.is)
+        getSquare[27].append(bombDown[27])
       }
     }
     if (guy2Y > 147 && guy2Y < 187) {
       if (guy2X < 60) {
-        getSquare[28].append(bomb.is)
+        getSquare[28].append(bombDown[28])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[29].append(bomb.is)
+        getSquare[29].append(bombDown[29])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[30].append(bomb.is)
+        getSquare[30].append(bombDown[30])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[31].append(bomb.is)
+        getSquare[31].append(bombDown[31])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[32].append(bomb.is)
+        getSquare[32].append(bombDown[32])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[33].append(bomb.is)
+        getSquare[33].append(bombDown[33])
       }
     }
     if (guy2Y > 187 && guy2Y < 227) {
       if (guy2X < 60){
-        getSquare[34].append(bomb.is)
+        getSquare[34].append(bombDown[34])
       }
       if (guy2X > 60 && guy2X < 107){
-          getSquare[35].append(bomb.is)
+          getSquare[35].append(bombDown[35])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[36].append(bomb.is)
+        getSquare[36].append(bombDown[36])
       }
       if (guy2X > 147 && guy2X < 187) {
-        getSquare[37].append(bomb.is)
+        getSquare[37].append(bombDown[37])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[38].append(bomb.is)
+        getSquare[38].append(bombDown[38])
       }
       if (guy2X > 227 && guy2X < 267) {
-        getSquare[39].append(bomb.is)
+        getSquare[39].append(bombDown[39])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[40].append(bomb.is)
+        getSquare[40].append(bombDown[40])
       }
       if (guy2X > 307 && guy2X < 347) {
-        getSquare[41].append(bomb.is)
+        getSquare[41].append(bombDown[41])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[42].append(bomb.is)
+        getSquare[42].append(bombDown[42])
       }
       if (guy2X > 387 && guy2X < 427) {
-        getSquare[43].append(bomb.is)
+        getSquare[43].append(bombDown[43])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[44].append(bomb.is)
+        getSquare[44].append(bombDown[44])
       }
     }
     if (guy2Y > 227 && guy2Y < 267) {
       if (guy2X < 60) {
-        getSquare[45].append(bomb.is)
+        getSquare[45].append(bombDown[45])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[46].append(bomb.is)
+        getSquare[46].append(bombDown[46])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[47].append(bomb.is)
+        getSquare[47].append(bombDown[47])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[48].append(bomb.is)
+        getSquare[48].append(bombDown[48])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[49].append(bomb.is)
+        getSquare[49].append(bombDown[49])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[50].append(bomb.is)
+        getSquare[50].append(bombDown[50])
       }
     }
     if (guy2Y > 267 && guy2Y < 307) {
       if (guy2X < 60){
-        getSquare[51].append(bomb.is)
+        getSquare[51].append(bombDown[51])
       }
       if (guy2X > 60 && guy2X < 107){
-          getSquare[52].append(bomb.is)
+          getSquare[52].append(bombDown[52])
       }
       if (guy2X > 107 && guy2X < 147) {
-        getSquare[53].append(bomb.is)
+        getSquare[53].append(bombDown[53])
       }
       if (guy2X > 147 && guy2X < 187) {
-        getSquare[54].append(bomb.is)
+        getSquare[54].append(bombDown[54])
       }
       if (guy2X > 187 && guy2X < 227) {
-        getSquare[55].append(bomb.is)
+        getSquare[55].append(bombDown[55])
       }
       if (guy2X > 227 && guy2X < 267) {
-        getSquare[56].append(bomb.is)
+        getSquare[56].append(bombDown[56])
       }
       if (guy2X > 267 && guy2X < 307) {
-        getSquare[57].append(bomb.is)
+        getSquare[57].append(bombDown[57])
       }
       if (guy2X > 307 && guy2X < 347) {
-        getSquare[58].append(bomb.is)
+        getSquare[58].append(bombDown[58])
       }
       if (guy2X > 347 && guy2X < 387) {
-        getSquare[59].append(bomb.is)
+        getSquare[59].append(bombDown[59])
       }
       if (guy2X > 387 && guy2X < 427) {
-        getSquare[60].append(bomb.is)
+        getSquare[60].append(bombDown[60])
       }
       if (guy2X > 427 && guy2X < 467) {
-        getSquare[61].append(bomb.is)
+        getSquare[61].append(bombDown[61])
       }
     }
+    $(".bomb").fadeOut(3000, function () {
+      $(".bomb").fadeIn(1, function (){
+        $(this).remove()
+      })
+    })
   }
 
   setTimeout (plantBomb, 50)
